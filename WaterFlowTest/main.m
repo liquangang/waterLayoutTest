2d8750d172e11852531be6885f27a5aa59f9e93e
liquangang <liquangang@quangangdembp>
2016-11-12 20:27:18 +0800

    Initial Commit

---
+++ WaterFlowTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WaterFlowTest
+//
+//  Created by liquangang on 2016/11/12.
+//  Copyright © 2016年 liquangang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
